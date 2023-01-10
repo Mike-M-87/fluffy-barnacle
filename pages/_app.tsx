8429ca1ebfe4b29e99ccfb3ff33070bccc64e370
Mike-M-87 <micminn87@gmail.com>
2023-01-10 20:46:15 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script src="https://kit.fontawesome.com/6be82c378c.js" crossOrigin="anonymous"></Script>
+    <Component {...pageProps} />
+  </>)
 }
